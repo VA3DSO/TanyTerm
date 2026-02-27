dd5506c828dd958ec94d782d8c64bf15bf69a72d
VA3DSO <rick@deepskies.com>
2026-02-27 15:00:31 -0500

    Added config file for expanded VIC, and added extra compile line to build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,10 @@
 set -e
 clear
 
+# for unxpanded VIC-20:
 cl65 -t vic20 --config vic20.cfg -Cl -O -o tt.prg tt.c
+
+# for 35K expanded VIC-20 (uncomment line below, and comment line above):
+# cl65 -t vic20 --config tt-35k.cfg -Cl -O -o tt.prg tt.c
+
 c1541 /home/rick/Sync/Computers/Commodore/Disks/VIC-20.dhd < inst.txt
